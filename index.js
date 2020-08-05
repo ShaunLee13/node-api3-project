4449c7b23db5537d7a1a8f61eb3e61700152c778
Shaun Lee <shaundlee.0103@gmail.com>
2020-08-05 15:04:46 -0400

    Added logger mw

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code away!
+const express = require('express')
+const helmet = require('helmet')
+
+const serverRouter = require('./server')
+const server = express()
+
+server.use(express.json())
+server.use(helmet())
+
+server.use('/', serverRouter)
+
+
+const port = 5000
+server.listen(port, () => {
+       console.log('Server Running on http://localhost:5000');
+})
